09bc0aaab3d296f1afabc92a4c3010baf1cbe627
João Ribeiro Bezerra <joaorb64@gmail.com>
2021-07-01 18:18:58 -0300

    Bugfix: text elements shrinked to fit container not getting back to full font-size

---
+++ layout/include/globals.js
@@ -0,0 +1,41 @@
+function getData() {
+    return $.ajax({
+        dataType: 'json',
+        url: '../../out/program_state.json',
+        cache: false,
+    });
+}
+
+function FitText(target) {
+    let textElement = target.find(".text");
+    textElement.css("font-size", "");
+    let fontSize = parseInt(target.css("font-size").split('px')[0]);
+
+    while(textElement.width() > parseInt(target.css("width").split('px')[0]) && fontSize > 0) {
+        fontSize--
+        textElement.css("font-size", fontSize+"px");
+    }
+}
+
+function SetInnerHtml(element, html, force=false){
+    let fadeOutTime = 0.5;
+    let fadeInTime = 0.5;
+
+    if(html == null) html = "";
+
+    // First run, no need of smooth fade out
+    if(element.find(".text").length == 0){
+        element.html("<div class='text'></div>");
+        fadeOutTime = 0;
+    };
+
+    html = html.replace(/'/g, '"');
+
+    if(force || element.find(".text").html() != html){
+        gsap.to(element.find(".text"), { autoAlpha: 0, duration: fadeOutTime, onComplete: ()=>{
+            element.find(".text").html(html);
+            FitText(element);
+            gsap.to(element.find(".text"), { autoAlpha: 1, duration: fadeInTime });
+        } });
+    }
+}
--- layout/scoreboard_simple/index.js
+++ layout/scoreboard_simple/index.js
@@ -1,45 +1,4 @@
 (($) => {
-    function getData() {
-        return $.ajax({
-            dataType: 'json',
-            url: '../../out/program_state.json',
-            cache: false,
-        });
-    }
-
-    function FitText(target) {
-        let fontSize = parseInt(target.css("font-size").split('px')[0]);
-        let textElement = target.find(".text");
-
-        while(textElement.width() > parseInt(target.css("width").split('px')[0]) && fontSize > 0) {
-            fontSize--
-            textElement.css("font-size", fontSize+"px");
-        }
-    }
-
-    function SetInnerHtml(element, html, force=false){
-        let fadeOutTime = 0.5;
-        let fadeInTime = 0.5;
-
-        if(html == null) html = "";
-
-        // First run, no need of smooth fade out
-        if(element.find(".text").length == 0){
-            element.html("<div class='text'></div>");
-            fadeOutTime = 0;
-        };
-
-        html = html.replace(/'/g, '"');
-
-        if(force || element.find(".text").html() != html){
-            gsap.to(element.find(".text"), { autoAlpha: 0, duration: fadeOutTime, onComplete: ()=>{
-                element.find(".text").html(html);
-                FitText(element);
-                gsap.to(element.find(".text"), { autoAlpha: 1, duration: fadeInTime });
-            } });
-        }
-    }
-    
     let startingAnimation = gsap.timeline({ paused: true })
         .from(['.phase'], { duration: .8, opacity: '0', y: "-20px", ease: "power2.inOut" }, 0)
             
