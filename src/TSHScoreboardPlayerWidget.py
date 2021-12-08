f6a91fe30dbf16518a7cc9db11529362d65f52ec
João Ribeiro Bezerra <joaorb64@gmail.com>
2021-12-08 15:53:46 -0300

    Fix

--- src/TSHScoreboardPlayerWidget.py
+++ src/TSHScoreboardPlayerWidget.py
@@ -232,8 +232,11 @@ class TSHScoreboardPlayerWidget(QGroupBox):
 
     def LoadStates(self, index):
         country: QComboBox = self.findChild(QComboBox, "country")
-        countryData = TSHScoreboardPlayerWidget.countries.get(country.currentData(
-            Qt.ItemDataRole.UserRole).get("code"), {})
+
+        countryData = None
+        if country.currentData(Qt.ItemDataRole.UserRole) != None:
+            countryData = TSHScoreboardPlayerWidget.countries[country.currentData(
+                Qt.ItemDataRole.UserRole).get("code")]
 
         stateModel = QStandardItemModel()
 
