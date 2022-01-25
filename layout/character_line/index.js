d7a4033549305dd6b4c98461b31bbfad957e63c8
João Ribeiro Bezerra <joaorb64@gmail.com>
2022-01-25 10:37:32 -0300

    Add initial character_line implementation

---
+++ layout/character_line/index.js
@@ -0,0 +1,53 @@
+(($) => { 
+    function Start(){
+        
+    }
+
+    var data = {}
+    var oldData = {}
+
+    let oldCharacters = {}
+
+    async function Update(){
+        oldData = data;
+        data = await getData();
+
+        let characters = data?.score?.team1?.players["1"]?.character;
+
+        if(JSON.stringify(characters) != JSON.stringify(oldCharacters)){
+            oldCharacters = characters;
+
+            assetToUse = "base_files/icon";
+            characterAssets = []
+    
+            Object.values(characters).forEach((character)=>{
+                if(character?.assets){
+                    if(character.assets.hasOwnProperty(assetToUse)){
+                        characterAssets.push(character.assets[assetToUse])
+                    }
+                }
+            })
+
+            let elements = "";
+
+            characterAssets.forEach((asset, i)=>{
+                elements += `<div class="icon" id="character${i}" style='background-image: url(../../${asset.asset})'></div>`;
+            })
+
+            elements += `<div class="index_display"></div>`
+    
+            $(".container").html(elements);
+
+            imgs = $.makeArray($('.icon'));
+        }
+
+    }
+
+    Update();
+    $(window).on("load", () => {
+        $('body').fadeTo(500, 1, async () => {
+            Start();
+            setInterval(Update, 1000);
+        });
+    });
+})(jQuery);
