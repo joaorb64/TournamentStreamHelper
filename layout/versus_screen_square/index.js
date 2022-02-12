4660b3167504aebc2c841ec923013b09f296631d
João Ribeiro Bezerra <joaorb64@gmail.com>
2022-02-12 12:22:12 -0300

    Start fixing vs screen square

--- layout/versus_screen_square/index.js
+++ layout/versus_screen_square/index.js
@@ -1,144 +1,216 @@
 (($) => {
-    // Change this to the name of the assets pack you want to use
-    // It's basically the folder name: assets/games/game/ASSETPACK
-    var ASSET_TO_USE = "full";
+  // Change this to the name of the assets pack you want to use
+  // It's basically the folder name: assets/games/game/ASSETPACK
+  var ASSET_TO_USE = "full";
 
-    // Change this to select wether to flip P2 character asset or not
-    // Set it to true or false
-    var FLIP_P2_ASSET = true;
+  // Change this to select wether to flip P2 character asset or not
+  // Set it to true or false
+  var FLIP_P2_ASSET = true;
 
-    let startingAnimation = gsap.timeline({ paused: true })
-        .from(['.phase'], { duration: .8, opacity: '0', ease: "power2.inOut" }, 0)
-        .from(['.score_container'], { duration: .8, opacity: '0', ease: "power2.inOut" }, 0)
-        .from(['.best_of'], { duration: .8, opacity: '0', ease: "power2.inOut" }, 0)
-        .from(['.vs'], { duration: .4, opacity: '0', scale: 4, ease: "out" }, 0.5)
-        .from(['.p1.character'], { duration: 1, x: "-200px", ease: "out" }, 0)
-        .from(['.p1.container'], { duration: 1, x: "-100px", ease: "out" }, 0)
-        .from(['.p2.character'], { duration: 1, x: "200px", ease: "out" }, 0)
-        .from(['.p2.container'], { duration: 1, x: "100px", ease: "out" }, 0)
-            
-    function Start(){
-        startingAnimation.restart();
-    }
+  let startingAnimation = gsap
+    .timeline({ paused: true })
+    .from([".phase"], { duration: 0.8, opacity: "0", ease: "power2.inOut" }, 0)
+    .from(
+      [".score_container"],
+      { duration: 0.8, opacity: "0", ease: "power2.inOut" },
+      0
+    )
+    .from(
+      [".best_of"],
+      { duration: 0.8, opacity: "0", ease: "power2.inOut" },
+      0
+    )
+    .from([".vs"], { duration: 0.4, opacity: "0", scale: 4, ease: "out" }, 0.5)
+    .from([".p1.container"], { duration: 1, x: "-100px", ease: "out" }, 0)
+    .from([".p2.container"], { duration: 1, x: "100px", ease: "out" }, 0);
 
-    var data = {}
-    var oldData = {}
+  function Start() {
+    startingAnimation.restart();
+  }
 
-    async function Update(){
-        oldData = data;
-        data = await getData();
+  var data = {};
+  var oldData = {};
 
-        ["p1", "p2"].forEach((p)=>{
-            SetInnerHtml($(`.${p} .name`), `
-                <span>
-                    <span class='sponsor'>
-                        ${data[p+"_org"] ? (data[p+"_org"]+"&nbsp;") : ""}
+  async function Update() {
+    oldData = data;
+    data = await getData();
+
+    Object.values(data.score.team).forEach((team, t) => {
+      Object.values(team.players).forEach((player, p) => {
+        SetInnerHtml(
+          $(`.p${t + 1} .name`),
+          `
+                    <span>
+                        <span class='sponsor'>
+                            ${player.team ? player.team + "&nbsp;" : ""}
+                        </span>
+                        ${player.name}
+                        ${team.losers ? " [L]" : ""}
                     </span>
-                    ${data[p+"_name"]}
-                    ${data[p+"_losers"] ? " [L]" : ""}
-                </span>
-            `);
+                `
+        );
 
-            SetInnerHtml($(`.${p} .sponsor_logo`),
-                data[p+"_org"] ? `
-                    <div>
-                        <div class='sponsor_logo' style='background-image: url(../../sponsor_logos/${data[p+"_org"].toUpperCase()}.png)'></div>
-                    </div>`
-                    :
-                    ""
-            , oldData[p+"_org"] != data[p+"_org"]);
-    
-            SetInnerHtml($(`.${p} .real_name`), `${data[p+"_real_name"]}`);
-    
-            SetInnerHtml($(`.${p} .twitter`), `
-                ${data[p+"_twitter"] ? `
-                    <div class="twitter_logo"></div>
-                    ${data[p+"_twitter"]}
-                    `
-                    :
-                    ""
-                }
-            `);
-    
-            SetInnerHtml($(`.${p} .flagcountry`),
-                data[p+"_country"] ? `
-                    <div>
-                        <div class='flag' style='background-image: url(../../assets/country_flag/${data[p+"_country"].toLowerCase()}.png)'>
-                            <div class="flagname">${data[p+"_country"].toUpperCase()}</div>
-                        </div>
-                    </div>`
-                    :
-                    ""
-            );
-    
-            SetInnerHtml($(`.${p} .flagstate`),
-                data[p+"_state"] ? `
-                    <div>
-                        <div class='flag' style='background-image: url(../../assets/state_flag/${data[p+"_country"].toUpperCase()}/${data[p+"_state"].toUpperCase()}.png)'>
-                            <div class="flagname">${data[p+"_state"].toUpperCase()}</div>
-                        </div>
-                    </div>`
-                    :
-                    ""
-            );
-    
-            if(oldData[p+"_character_codename"] != data[p+"_character_codename"] ||
-            oldData[p+"_character_color"] != data[p+"_character_color"]){
-                if(!data[p+"_assets_path"][ASSET_TO_USE].endsWith(".webm")){
-                    // if asset is a image, add a image element
-                    $(`.${p}.character`).html(`
-                        <div class="bg">
-                            <div
-                                class="portrait"
-                                style='
-                                    background-image: url(../../${data[p+"_assets_path"][ASSET_TO_USE]});
-                                    ${p == "p2" && FLIP_P2_ASSET ? "transform: scaleX(-1)" : ""}
-                                '>
-                            </div>
-                        </div>
-                    `)
-                } else {
-                    // if asset is a video, add a video element
-                    $(`.${p}.character`).html(`
-                        <div class="bg">
-                            <video id="video_${p}" class="video" width="auto" height="100%" autoplay muted>
-                                <source src="../../${data[p+"_assets_path"][ASSET_TO_USE]}">
-                            </video>
-                        </div>
-                    `)
-                }
-                
-                gsap.timeline()
-                    .from(
-                        `.${p}.character .portrait`,
-                        {duration: .5, opacity: 0}
-                    )
-                    .from(
-                        `.${p}.character .portrait`,
-                        { duration: .4, filter: 'brightness(0%)', onUpdate: function(tl) {
-                            var tlp = (this.progress() * 100) >> 0;
-                            TweenMax.set(`.${p}.character .portrait`, {'filter': 'brightness(' + tlp + '%)'});
-                        },
-                        onUpdateParams: ["{self}"] }
-                    )
+        SetInnerHtml(
+          $(`.${p} .sponsor_logo`),
+          data[p + "_org"]
+            ? `
+                        <div>
+                            <div class='sponsor_logo' style='background-image: url(../../sponsor_logos/${data[
+                              p + "_org"
+                            ].toUpperCase()}.png)'></div>
+                        </div>`
+            : "",
+          oldData[p + "_org"] != data[p + "_org"]
+        );
+
+        SetInnerHtml($(`.p${t + 1} .real_name`), `${player.real_name}`);
+
+        SetInnerHtml(
+          $(`.p${t + 1} .twitter`),
+          `
+                    ${
+                      player.twitter
+                        ? `
+                        <div class="twitter_logo"></div>
+                        ${player.twitter}
+                        `
+                        : ""
+                    }
+                `
+        );
+
+        SetInnerHtml(
+          $(`.p${t + 1} .flagcountry`),
+          player.country
+            ? `
+            <div>
+                <div class='flag' style='background-image: url(../../${player.country.asset});'>
+                    <div class="flagname">${player.country.code}</div>
+                </div>
+            </div>`
+            : ""
+        );
+
+        SetInnerHtml(
+          $(`.p${t + 1} .flagstate`),
+          player.state
+            ? `
+            <div>
+                <div class='flag' style='background-image: url(../../${player.state.asset});'>
+                    <div class="flagname">${player.state.code}</div>
+                </div>
+            </div>`
+            : ""
+        );
+
+        if (
+          !oldData.score ||
+          JSON.stringify(player.character) !=
+            JSON.stringify(
+              oldData.score.team[String(t + 1)].players[String(p + 1)].character
+            )
+        ) {
+          let html = "";
+          let characters = Object.values(player.character);
+          if (t == 0) characters = characters.reverse();
+          let zIndexMultiplyier = 1;
+          if (t == 1) zIndexMultiplyier = -1;
+          characters.forEach((character, c) => {
+            if (character) {
+              if (!character.assets[ASSET_TO_USE].asset.endsWith(".webm")) {
+                // if asset is a image, add a image element
+                html += `
+                <div class="bg char${
+                  t == 1 ? c : characters.length - 1 - c
+                }" style="z-index: ${c * zIndexMultiplyier};">
+                  <div
+                    class="portrait"
+                    style='
+                        background-image: url(../../${
+                          character.assets[ASSET_TO_USE].asset
+                        });
+                        ${
+                          t == 1 && FLIP_P2_ASSET ? "transform: scaleX(-1)" : ""
+                        }
+                    '>
+                    </div>
+                </div>
+                  `;
+              } else {
+                // if asset is a video, add a video element
+                html += `
+                <div class="bg char${
+                  t == 1 ? c : characters.length - 1 - c
+                }" style="z-index: ${c * zIndexMultiplyier};">
+                  <video id="video_${p}" class="video" width="auto" height="100%" autoplay muted>
+                    <source src="../../${character.assets[ASSET_TO_USE].asset}">
+                  </video>
+                </div>
+                  `;
+              }
+
+              // if (character.assets["full"]) {
+              //   charactersHtml += `
+              //                 <div class='character' style='background-image: url(../../${character.assets["full"].asset})'></div>
+              //             `;
+              // }
             }
-    
-        })
+          });
 
-        SetInnerHtml($(`.p1 .score`), String(data.score_left));
-        SetInnerHtml($(`.p2 .score`), String(data.score_right));
+          $(`.p${t + 1}.character`).html(html);
 
-        SetInnerHtml($(".phase"), data.tournament_phase);
-        SetInnerHtml($(".best_of"), data.best_of ? "Best of "+data.best_of : "");
-    }
+          characters.forEach((character, c) => {
+            if (character) {
+              gsap.timeline().from(
+                [`.p${t + 1}.character .char${c}`],
+                {
+                  duration: 0.8,
+                  x: zIndexMultiplyier * -800 + "px",
+                  ease: "out",
+                },
+                c / 6
+              );
+            }
+          });
 
-    // Using update here to set images as soon as possible
-    // so that on window.load they are already preloaded
-    Update();
-    $(window).on("load", () => {
-        $('body').fadeTo(0, 1, async () => {
-            Start();
-            setInterval(Update, 1000);
-        });
+          gsap
+            .timeline()
+            .from(`.p${t + 1}.character .portrait`, {
+              duration: 0.5,
+              opacity: 0,
+            })
+            .from(`.p${t + 1}.character .portrait`, {
+              duration: 0.4,
+              filter: "brightness(0%)",
+              onUpdate: function (tl) {
+                var tlp = (this.progress() * 100) >> 0;
+                TweenMax.set(`.p${t + 1}.character .portrait`, {
+                  filter: "brightness(" + tlp + "%)",
+                });
+              },
+              onUpdateParams: ["{self}"],
+            });
+        }
+      });
     });
+
+    SetInnerHtml($(`.p1 .score`), String(data.score.team["1"].score));
+    SetInnerHtml($(`.p2 .score`), String(data.score.team["2"].score));
+
+    SetInnerHtml($(".phase"), data.score.match);
+    SetInnerHtml(
+      $(".best_of"),
+      data.score.best_of ? "Best of " + data.score.best_of : ""
+    );
+  }
+
+  // Using update here to set images as soon as possible
+  // so that on window.load they are already preloaded
+  Update();
+  $(window).on("load", () => {
+    $("body").fadeTo(0, 1, async () => {
+      Start();
+      setInterval(Update, 1000);
+    });
+  });
 })(jQuery);
