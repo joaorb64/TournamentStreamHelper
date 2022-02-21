9d5e4ed9942c643eabb78b46baddafc186e6f8e8
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2022-02-21 23:45:32 +0100

    Fixed an issue where alpha compositing wasn't done properly

--- src/generate_thumbnail.py
+++ src/generate_thumbnail.py
@@ -1,6 +1,7 @@
 # This script can be used to generate thumbnails using ./out/program_state.json and ./thumbnail_base
 # Run as python ./src/generate_thumbnail in order to test it
 
+from turtle import back
 from PIL import Image, ImageFont, ImageDraw
 from pathlib import Path
 import json
@@ -63,6 +64,10 @@ def calculate_new_dimensions(current_size, max_size):
         new_y = x_ratio*current_size[1]
     return((round(new_x), round(new_y)))
 
+def create_composite_image(image, size, coordinates):
+    background = Image.new('RGBA', size, (0,0,0,0))
+    background.paste(image, coordinates, image)
+    return(background)
 
 def paste_characters(thumbnail, data):
     used_assets = "full"
@@ -77,14 +82,17 @@ def paste_characters(thumbnail, data):
         team_index = i+1
         current_image_path = find(
             f"score.team.{team_index}.players.1.character.1.assets.{used_assets}.asset", data)
-        character_image = Image.open(current_image_path)
+        character_image = Image.open(current_image_path).convert('RGBA')
         new_size = calculate_new_dimensions(character_image.size, max_size)
         character_image = character_image.resize(
             new_size, resample=Image.BICUBIC)
         paste_x = round((max_x_size - new_size[0])/2) + origin_x_coordinates[i]
         paste_y = round((max_y_size - new_size[1])/2) + origin_y_coordinates[i]
         paste_coordinates = (paste_x, paste_y)
-        thumbnail.paste(character_image, paste_coordinates, character_image)
+        composite_image = create_composite_image(character_image, thumbnail.size, paste_coordinates)
+        thumbnail = Image.alpha_composite(thumbnail, composite_image)
+
+    return(thumbnail)
 
 
 def get_text_size_for_height(thumbnail, font_path, pixel_height, search_interval=None, recursion_level=0):
@@ -178,30 +186,35 @@ def paste_icon(thumbnail, icon_path):
     max_y_size = round(thumbnail.size[1]*(200.0/1080.0))
     max_size = (max_x_size, max_y_size)
 
-    icon_image = Image.open(icon_path)
+    icon_image = Image.open(icon_path).convert('RGBA')
     icon_size = calculate_new_dimensions(icon_image.size, max_size)
     icon_image = icon_image.resize(icon_size, resample=Image.BICUBIC)
 
     icon_x = round(thumbnail.size[0]/2 - icon_size[0]/2)
     icon_y = round(thumbnail.size[1]*(6.0/1080.0))
     icon_coordinates = (icon_x, icon_y)
-    thumbnail.paste(icon_image, icon_coordinates, icon_image)
+    composite_image = create_composite_image(icon_image, thumbnail.size, icon_coordinates)
+    thumbnail = Image.alpha_composite(thumbnail, composite_image)
+    return(thumbnail)
 
 
 Path(out_path).mkdir(parents=True, exist_ok=True)
 
-foreground = Image.open(foreground_path)
-background = Image.open(background_path)
+foreground = Image.open(foreground_path).convert('RGBA')
+background = Image.open(background_path).convert('RGBA')
 with open(data_path, 'rt', encoding='utf-8') as f:
     data = json.loads(f.read())
 
 thumbnail = Image.new("RGBA", foreground.size, "PINK")
-thumbnail.paste(background, (0, 0), background)
-paste_characters(thumbnail, data)
-thumbnail.paste(foreground, (0, 0), foreground)
+composite_image = create_composite_image(background, thumbnail.size, (0,0))
+thumbnail = Image.alpha_composite(thumbnail, composite_image)
+thumbnail.paste(background, (0, 0), mask=background)
+thumbnail = paste_characters(thumbnail, data)
+composite_image = create_composite_image(foreground, thumbnail.size, (0,0))
+thumbnail = Image.alpha_composite(thumbnail, composite_image)
 paste_player_text(thumbnail, data)
 paste_round_text(thumbnail, data, display_phase)
-paste_icon(thumbnail, icon_path)
+thumbnail = paste_icon(thumbnail, icon_path)
 
 thumbnail_filename = f"thumb-{datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')}.png"
 thumbnail.save(f"{out_path}/{thumbnail_filename}")
