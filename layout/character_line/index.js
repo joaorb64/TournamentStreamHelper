8777429d6a009a2b319052455c904be1873a3e37
João Ribeiro Bezerra <joaorb64@gmail.com>
2022-03-11 08:59:21 -0300

    Fix eyesight centering on character_line

--- layout/character_line/index.js
+++ layout/character_line/index.js
@@ -10,18 +10,26 @@
     oldData = data;
     data = await getData();
 
-    let characters = data.score.team["1"].player["1"].character;
+    let team = window.team ? window.team : 1;
+    let player = window.player ? window.player : 1;
+
+    let characters = data.score.team[team].player[player].character;
 
     if (JSON.stringify(characters) != JSON.stringify(oldCharacters)) {
+      console.log(oldCharacters);
+      console.log(characters);
       oldCharacters = characters;
 
-      assetToUse = "base_files/icon";
+      assetToUse = "full";
       characterAssets = [];
 
       Object.values(characters).forEach((character) => {
         if (character.assets) {
           if (character.assets.hasOwnProperty(assetToUse)) {
-            characterAssets.push(character.assets[assetToUse]);
+            let asset = character.assets[assetToUse];
+            asset.img = new Image();
+            asset.img.src = "../../" + asset.asset;
+            characterAssets.push(asset);
           } else {
             characterAssets.push({});
           }
@@ -31,14 +39,47 @@
       let elements = "";
 
       characterAssets.forEach((asset, i) => {
-        elements += `<div class="icon" id="character${i}" style='background-image: url(../../${asset.asset})'></div>`;
+        elements += `
+          <div
+            class="icon"
+            id="character${i}"
+            style="background-image: url('../../${asset.asset}')"
+          ></div>
+        `;
       });
 
       elements += `<div class="index_display"></div>`;
 
       $(".container").html(elements);
 
-      imgs = $.makeArray($(".icon"));
+      characterAssets.forEach((a, i) => {
+        let img = a.img;
+
+        let reposition = () => {
+          let proportion = img.naturalWidth / img.naturalHeight;
+          $(`#character${i}`).css(
+            "background-position",
+            `${
+              ((characterAssets[i].eyesight.x * 1) /
+                proportion /
+                img.naturalWidth) *
+              100
+            }% ${
+              ((characterAssets[i].eyesight.y * proportion) /
+                img.naturalHeight) *
+              100
+            }%`
+          );
+        };
+
+        $(window).resize(() => {
+          reposition();
+        });
+
+        $(img).on("load", () => {
+          reposition();
+        });
+      });
     }
   }
 
