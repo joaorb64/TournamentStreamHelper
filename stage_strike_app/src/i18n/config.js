367dc63514ffd8bd8b7f5e2e85e08f4d05b6f8ef
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2022-04-10 18:48:44 +0200

    Enabled French localisation

--- stage_strike_app/src/i18n/config.js
+++ stage_strike_app/src/i18n/config.js
@@ -16,6 +16,9 @@ i18n.use(LanguageDetector).init({
     ja: {
       translations: require("./locales/jp.json"),
     },
+    fr: {
+      translations: require("./locales/fr.json"),
+    },
   },
   detection: {
     order: ["querystring", "navigator"],
