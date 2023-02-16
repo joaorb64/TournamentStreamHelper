95543cc1547bbb477064b4b5e1b5f2aeba4c9234
João Ribeiro Bezerra <joaorb64@gmail.com>
2023-02-16 11:51:52 +0000

    Revert "Fix crash on invalid tournament"

--- src/TSHTournamentInfoWidget.py
+++ src/TSHTournamentInfoWidget.py
@@ -9,7 +9,6 @@ from .StateManager import StateManager
 from .TSHGameAssetManager import TSHGameAssetManager
 from .TSHPlayerDB import TSHPlayerDB
 from .TSHTournamentDataProvider import TSHTournamentDataProvider
-import traceback
 
 
 class TSHTournamentInfoWidget(QDockWidget):
@@ -60,27 +59,24 @@ class TSHTournamentInfoWidget(QDockWidget):
                 widget.setValue(0)
 
         for key in data.keys():
-            try:
-                widget: QWidget = self.findChild(QWidget, key)
+            widget: QWidget = self.findChild(QWidget, key)
 
-                if widget:
-                    if type(widget) == QLineEdit:
-                        widget.setText(data[key])
-                        widget.editingFinished.emit()
+            if widget:
+                if type(widget) == QLineEdit:
+                    widget.setText(data[key])
+                    widget.editingFinished.emit()
 
-                    if type(widget) == QSpinBox:
-                        widget.setValue(data[key])
+                if type(widget) == QSpinBox:
+                    widget.setValue(data[key])
 
-                    if type(widget) == QDateEdit:
-                        # This is how to put a date into QDateEdit Element using a timestamp
-                        #        aTime = '2018-07-24 19:24:11.272000'
-                        unix_timestamp = float(data[key])
-                        #        tmToDate = datetime.datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-                        #        utc_time = time.gmtime(unix_timestamp)
-                        local_datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(
-                            unix_timestamp))  # converts timestampt to local time and apply the format
-                        local_date = local_datetime.split(" ")[0]  # get only the date
-                        d = QDate.fromString(local_date, "yyyy-MM-dd")  # Convert date into QDate object
-                        widget.setDate(d)
-            except:
-                print(traceback.format_exc())
+                if type(widget) == QDateEdit:
+                    # This is how to put a date into QDateEdit Element using a timestamp
+                    #        aTime = '2018-07-24 19:24:11.272000'
+                    unix_timestamp = float(data[key])
+                    #        tmToDate = datetime.datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+                    #        utc_time = time.gmtime(unix_timestamp)
+                    local_datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(
+                        unix_timestamp))  # converts timestampt to local time and apply the format
+                    local_date = local_datetime.split(" ")[0]  # get only the date
+                    d = QDate.fromString(local_date, "yyyy-MM-dd")  # Convert date into QDate object
+                    widget.setDate(d)
