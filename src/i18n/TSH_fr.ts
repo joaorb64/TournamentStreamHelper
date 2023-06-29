55d0a7ba6a1d210e715db7a168cc22c72e9b8340
GitHub Action <action@github.com>
2023-06-29 08:21:07 +0000

    Update locale files

--- src/i18n/TSH_fr.ts
+++ src/i18n/TSH_fr.ts
@@ -1437,7 +1437,7 @@ p, li { white-space: pre-wrap; }
         <translation>Raccourcis clavier</translation>
     </message>
     <message>
-        <location filename="../Settings/SettingsWidget.py" line="26"/>
+        <location filename="../Settings/SettingsWidget.py" line="28"/>
         <source>Default</source>
         <translation>Paramètres par défaut</translation>
     </message>
