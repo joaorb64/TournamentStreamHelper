314de70602792bd5f30ee596b1d7b2f84e095c61
João Ribeiro Bezerra <joaorb64@gmail.com>
2023-10-10 20:51:29 +0100

    Lock export updates when adding/removing scoreboards

--- src/TSHScoreboardManager.py
+++ src/TSHScoreboardManager.py
@@ -6,16 +6,17 @@ from loguru import logger
 from .TSHScoreboardWidget import TSHScoreboardWidget
 from .StateManager import StateManager
 
+
 class TSHScoreboardManagerSignals(QObject):
     ScoreboardAmountChanged = Signal(int)
+
 
 class TSHScoreboardManager(QDockWidget):
     instance: "TSHScoreboardManager" = None
 
-
     def __init__(self, *args):
         super().__init__(*args)
-        
+
         StateManager.Unset("score")
 
         self.signals: TSHScoreboardManagerSignals = TSHScoreboardManagerSignals()
@@ -37,47 +38,58 @@ class TSHScoreboardManager(QDockWidget):
         self.scoreboardholder = []
 
     def UpdateAmount(self, amount):
-        if amount > len(self.scoreboardholder):
-            logger.info("Scoreboard Manager - Creating Scoreboard " + str(amount))
+        StateManager.BlockSaving()
 
-            if int(amount)-1 == 1:
-                self.GetScoreboard(1).btLoadPlayerSet.setHidden(True)
-                self.GetScoreboard(1).btLoadPlayerSetOptions.setHidden(True)
-            
-            scoreboard = QWidget()
-            scoreboard.setLayout(QVBoxLayout())
-            scoreboardObj = TSHScoreboardWidget(scoreboardNumber=amount)
-            self.scoreboardholder.append(scoreboardObj)
-            scoreboard.layout().addWidget(scoreboardObj)
-            self.tabs.addTab(scoreboard
-                 , QApplication.translate("app", "Scoreboard") + " " + str(amount))
-        else:
-            logger.info("Scoreboard Manager - Removing Scoreboard " + str(amount+1))
-            self.tabs.removeTab(amount)
-            self.scoreboardholder[amount].deleteLater()
-            self.scoreboardholder.pop(amount)
-            if int(amount) == 1:
-                self.GetScoreboard(1).btLoadPlayerSet.setHidden(False)
-                self.GetScoreboard(1).btLoadPlayerSetOptions.setHidden(False)
-            StateManager.Unset(f"score.{amount+1}")
+        try:
+            if amount > len(self.scoreboardholder):
+                logger.info(
+                    "Scoreboard Manager - Creating Scoreboard " + str(amount))
+
+                if int(amount)-1 == 1:
+                    self.GetScoreboard(1).btLoadPlayerSet.setHidden(True)
+                    self.GetScoreboard(
+                        1).btLoadPlayerSetOptions.setHidden(True)
+
+                scoreboard = QWidget()
+                scoreboard.setLayout(QVBoxLayout())
+                scoreboardObj = TSHScoreboardWidget(scoreboardNumber=amount)
+                self.scoreboardholder.append(scoreboardObj)
+                scoreboard.layout().addWidget(scoreboardObj)
+                self.tabs.addTab(scoreboard, QApplication.translate(
+                    "app", "Scoreboard") + " " + str(amount))
+            else:
+                logger.info(
+                    "Scoreboard Manager - Removing Scoreboard " + str(amount+1))
+                self.tabs.removeTab(amount)
+                self.scoreboardholder[amount].deleteLater()
+                self.scoreboardholder.pop(amount)
+                if int(amount) == 1:
+                    self.GetScoreboard(1).btLoadPlayerSet.setHidden(False)
+                    self.GetScoreboard(
+                        1).btLoadPlayerSetOptions.setHidden(False)
+                StateManager.Unset(f"score.{amount+1}")
+        finally:
+            StateManager.ReleaseSaving()
 
     def GetScoreboard(self, number):
         if int(number)-1 < len(self.scoreboardholder):
             return self.scoreboardholder[int(number)-1]
         else:
-            logger.error(f"Scoreboard Manager - Unable to retrieve scoreboard {number}, defaulting to scoreboard 1")
+            logger.error(
+                f"Scoreboard Manager - Unable to retrieve scoreboard {number}, defaulting to scoreboard 1")
             return self.scoreboardholder[0]
-        
+
     def SetTabName(self, index, name):
         if int(index)-1 < self.tabs.count():
             if name != "":
                 self.tabs.setTabText(int(index)-1, name)
             else:
                 self.tabs.setTabText(int(index)-1,
-                    QApplication.translate("app", "Scoreboard") + " " + str(index))
+                                     QApplication.translate("app", "Scoreboard") + " " + str(index))
         else:
             logger.error(f"Invalid Scoreboard ID provided: {index}")
-            logger.error(f"Please provide an ID between 1 and {self.tabs.count()}")
+            logger.error(
+                f"Please provide an ID between 1 and {self.tabs.count()}")
 
     def GetTabAmount(self):
         return self.tabs.count()
