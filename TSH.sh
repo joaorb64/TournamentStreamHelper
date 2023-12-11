4440f5cfff213403c34b3063f3a1880ff85008f3
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2023-12-11 15:53:28 +0100

    Changed SH script to use virtual environments

--- TSH.sh
+++ TSH.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 -m pip install -r dependencies/requirements.txt
-python3 main.py
+python3 -m pip install pipenv
+python3 -m pipenv install -r dependencies/requirements.txt
+python3 -m pipenv run python main.py
