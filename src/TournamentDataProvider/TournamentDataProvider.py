1d874dc1a3a875f862a7b2e5f790f0d0a1baf5d7
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2024-07-04 22:41:27 +0200

    Fixed issue where youtu.be link was not being processed properly

--- src/TournamentDataProvider/TournamentDataProvider.py
+++ src/TournamentDataProvider/TournamentDataProvider.py
@@ -77,8 +77,13 @@ class TournamentDataProvider:
             stream = stream.split("twitch.tv/")[1].replace("/", "")
             stream = f"Twitch: {stream}"
         if "youtube.com" in stream or "youtu.be" in stream:
+            if "youtu.be" in stream: # Convert stream URL if shortened
+                stream = stream.split("?")[0]
+                stream = f"https://youtube.com/watch?v={stream.split("/")[-1]}"
+
             data_server_search = requests.get("https://api.invidious.io/instances.json?pretty=1&sort_by=health") # Use 3rd Party Client to get YouTube info
             if int(data_server_search.status_code) == 200:
+                # Filter data servers which are up and reliable
                 possible_data_server_list = []
                 for data_server_info in orjson.loads(data_server_search.text):
                     if data_server_info[1].get("api") \
