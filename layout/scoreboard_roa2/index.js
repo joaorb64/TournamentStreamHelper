e30c25b2b13d3d4d968a7e5aeb4d28f435346521
João Ribeiro Bezerra <joaorb64@gmail.com>
2024-11-01 22:29:22 +0000

    Add Shino's ROA scoreboard

---
+++ layout/scoreboard_roa2/index.js
@@ -0,0 +1,337 @@
+LoadEverything().then(() => {
+  
+  gsap.config({ nullTargetWarn: false, trialWarn: false });
+
+  let startingAnimation = gsap
+    .timeline({ paused: true })
+    .from(
+      [".fade"],
+      {
+        duration: 0.2,
+        autoAlpha: 0,
+        ease: "power2.out",
+      },
+      0
+    )
+    .from(
+      [".fade_down_left_stagger:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'start',
+          opacity: 0,
+          y: "-20px",
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".fade_down_right_stagger:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'start',
+          opacity: 0,
+          y: "-20px",
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".p1 .fade_stagger:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'end',
+          opacity: 0,
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".p2 .fade_stagger:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'end',
+          opacity: 0,
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".p1 .fade_stagger_reverse:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'start',
+          opacity: 0,
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".p2 .fade_stagger_reverse:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'start',
+          opacity: 0,
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".fade_right_stagger:not(.text_empty)"],
+      {
+        autoAlpha: 0,
+        stagger: {
+          each: 0.05,
+          from: 'end',
+          opacity: 0,
+        },
+        duration: 0.2,
+      },
+      0
+    )
+    .from(
+      [".fade_down"],
+      {
+        duration: 0.2,
+        y: "-20px",
+        ease: "power2.out",
+        autoAlpha: 0,
+      },
+      0
+    )
+    .from(
+      [".fade_right"],
+      {
+        duration: 0.2,
+        x: "-20px",
+        ease: "power2.out",
+        autoAlpha: 0,
+      },
+      0
+    )
+    .from(
+      [".fade_left"],
+      {
+        duration: 0.2,
+        x: "+20px",
+        ease: "power2.out",
+        autoAlpha: 0,
+      },
+      0
+    )
+    .from(
+      [".fade_up"],
+      {
+        duration: 0.2,
+        y: "+20px",
+        ease: "power2.out",
+        autoAlpha: 0,
+      },
+      0
+    )
+
+  Start = async () => {
+    startingAnimation.restart();
+  };
+
+  Update = async (event) => {
+    let data = event.data;
+    let oldData = event.oldData;
+
+    let isTeams = Object.keys(data.score[window.scoreboardNumber].team["1"].player).length > 1;
+
+    if (!isTeams) {
+      for (const [t, team] of [
+        data.score[window.scoreboardNumber].team["1"],
+        data.score[window.scoreboardNumber].team["2"],
+      ].entries()) {
+        for (const [p, player] of [team.player["1"]].entries()) {
+          if (player) {
+            SetInnerHtml(
+              $(`.p${t + 1}.container .name`),
+              `
+                <span class="sponsor">
+                  ${player.team ? player.team : ""}
+                </span>
+                ${await Transcript(player.name)}
+              `
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1}.losers_container`),
+              team.losers? "<div class='losers container'>L</div>" : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1} .flagcountry`),
+              player.country.asset
+                ? `
+                  <div class='flag' style='background-image: url(../../${player.country.asset.toLowerCase()})'></div>
+                  <div>${player.country.code}</div>
+                `
+                : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1} .flagstate`),
+              player.state.asset
+                ? `
+                  <div class='flag' style='background-image: url(../../${player.state.asset})'></div>
+                  <div>${player.state.code}</div>
+                `
+                : ""
+            );
+
+            await CharacterDisplay(
+              $(`.p${t + 1}.container .character_container`),
+              {
+                source: `score.${window.scoreboardNumber}.team.${t + 1}`,
+              },
+              event
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1}.container .sponsor_icon`),
+              player.sponsor_logo
+                ? `<div style='background-image: url(../../${player.sponsor_logo})'></div>`
+                : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1}.container .avatar`),
+              player.avatar
+                ? `<div style="background-image: url('../../${player.avatar}')"></div>`
+                : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1}.container .online_avatar`),
+              player.online_avatar
+                ? `<div style="background-image: url('${player.online_avatar}')"></div>`
+                : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1} .twitter`),
+              player.twitter
+                ? `<span class="twitter_logo"></span>${String(player.twitter)}`
+                : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1} .pronoun`),
+              player.pronoun ? player.pronoun : ""
+            );
+
+            SetInnerHtml(
+              $(`.p${t + 1} .seed`),
+              player.seed ? `Seed ${player.seed}` : ""
+            );
+
+            SetInnerHtml($(`.p${t + 1}.container .score`), String(team.score));
+            SetInnerHtml($(`.p${t + 1}.floating_score`), String(team.score));
+
+            SetInnerHtml(
+              $(`.p${t + 1}.container .sponsor-container`),
+              `<div class='sponsor-logo' style='background-image: url(../../${player.sponsor_logo})'></div>`
+            );
+          }
+        }
+        if(team.color && !tsh_settings["forceDefaultScoreColors"]) {
+          document.querySelector(':root').style.setProperty(`--p${t + 1}-score-bg-color`, team.color);
+        }
+      }
+    } else {
+      for (const [t, team] of [
+        data.score[window.scoreboardNumber].team["1"],
+        data.score[window.scoreboardNumber].team["2"],
+      ].entries()) {
+        let teamName = team.teamName;
+
+        let names = [];
+        for (const [p, player] of Object.values(team.player).entries()) {
+          if (player && player.name) {
+            names.push(await Transcript(player.name));
+          }
+        }
+        let playerNames = names.join(" / ");
+
+        if (!team.teamName || team.teamName == "") {
+          teamName = playerNames;
+        }
+
+        SetInnerHtml(
+          $(`.p${t + 1}.container .name`),
+          `
+            ${teamName}
+          `
+        );
+
+        SetInnerHtml(
+          $(`.p${t + 1}.losers_container`),
+          team.losers? "<div class='losers container'>L</div>" : ""
+        );
+
+        SetInnerHtml($(`.p${t + 1} .flagcountry`), "");
+
+        SetInnerHtml($(`.p${t + 1} .flagstate`), "");
+
+        await CharacterDisplay(
+          $(`.p${t + 1}.container .character_container`),
+          {
+            source: `score.${window.scoreboardNumber}.team.${t + 1}`,
+            slice_character: [0, 1],
+          },
+          event
+        );
+
+        SetInnerHtml($(`.p${t + 1}.container .sponsor_icon`), "");
+
+        SetInnerHtml($(`.p${t + 1}.container .avatar`), "");
+
+        SetInnerHtml($(`.p${t + 1}.container .online_avatar`), "");
+
+        SetInnerHtml($(`.p${t + 1} .twitter`), 
+          playerNames != team.teamName ? playerNames : ""
+        );
+
+        SetInnerHtml($(`.p${t + 1}.container .score`), String(team.score));
+        SetInnerHtml($(`.p${t + 1}.floating_score`), String(team.score));
+
+        SetInnerHtml($(`.p${t + 1}.container .sponsor-container`), "");
+
+        if(team.color) {
+          document.querySelector(':root').style.setProperty(`--p${t + 1}-score-bg-color`, team.color);
+        }
+      }
+    }
+
+    SetInnerHtml($(".tournament_name"), data.tournamentInfo.tournamentName);
+    SetInnerHtml($(".event_name"), data.tournamentInfo.eventName);
+
+    SetInnerHtml($(".match"), data.score[window.scoreboardNumber].match);
+
+    let phaseTexts = [];
+    if (data.score[window.scoreboardNumber].phase) phaseTexts.push(data.score[window.scoreboardNumber].phase);
+    if (data.score[window.scoreboardNumber].best_of_text) phaseTexts.push(data.score[window.scoreboardNumber].best_of_text);
+
+    SetInnerHtml($(".phase"), phaseTexts.join(" - "));
+  };
+});
