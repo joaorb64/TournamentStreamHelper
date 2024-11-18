c206576fa4015c14eea7cbbdaf8da58c07cc39c1
Isatis <isatis@foxes.gay>
2024-11-17 17:42:17 -0800

    Switch releases to zip, prefer release.zip if it exists and source if not

--- scripts/gen_release.ps1
+++ scripts/gen_release.ps1
@@ -1,4 +1,26 @@
 # Zip Files using PowerShell
 Push-Location (Join-Path $PWD "..")
-Compress-Archive -Path "assets", "layout", "user_data", "LICENSE", "TSH.exe" -DestinationPath "release.zip"
+
+$excludeFiles = @(
+    "assets\versions.json",
+    "assets\contributors.txt"
+)
+
+$sourcePaths = @(
+    "assets",
+    "layout",
+    "user_data",
+    "LICENSE",
+    "TSH.exe"
+)
+
+# Create a temporary list of files to include, excluding specific files
+$filesToInclude = Get-ChildItem -Path $sourcePaths -Recurse | Where-Object {
+    $excludeFiles -notcontains $_.FullName
+}
+
+# Compress the filtered files into a zip archive
+$zipPath = "release.zip"
+Compress-Archive -Path $filesToInclude -DestinationPath $zipPath
+
 Pop-Location
--- scripts/gen_release.sh
+++ scripts/gen_release.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-pushd $PWD/..
-zip -r release.zip assets layout user_data LICENSE TSH.exe
+pushd ..
+
+mkdir TournamentStreamHelper
+cp -R assets \
+	layout \
+	user_data \
+	LICENSE \
+	TSH.exe \
+	TournamentStreamHelper/
+
+zip -rv \
+    release.zip \
+    TournamentStreamHelper/assets -x \
+    	"TournamentStreamHelper/assets/versions.json" \
+    	"TournamentStreamHelper/assets/contributors.txt" \
+    TournamentStreamHelper/layout \
+    TournamentStreamHelper/user_data \
+    TournamentStreamHelper/LICENSE \
+    TournamentStreamHelper/TSH.exe
+
+rm -rf TournamentStreamHelper
 popd
