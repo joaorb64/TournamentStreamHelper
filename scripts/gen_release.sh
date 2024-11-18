ba0316bc999e385ec8d769b47fc957589ac9940d
Isatis <isatis@foxes.gay>
2024-11-17 22:31:35 -0800

    Put all files inside one folder then zip the folder

--- scripts/gen_release.sh
+++ scripts/gen_release.sh
@@ -12,13 +12,10 @@ cp -R assets \
 
 zip -rv \
     release.zip \
-    TournamentStreamHelper/assets -x \
+    	TournamentStreamHelper \
+    -x \
     	"TournamentStreamHelper/assets/versions.json" \
     	"TournamentStreamHelper/assets/contributors.txt" \
-    TournamentStreamHelper/layout \
-    TournamentStreamHelper/user_data \
-    TournamentStreamHelper/LICENSE \
-    TournamentStreamHelper/TSH.exe
 
 rm -rf TournamentStreamHelper
 popd
