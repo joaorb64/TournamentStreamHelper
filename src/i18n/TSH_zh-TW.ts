1124dc5b3ac373ff16d3dc5594d066b8d5c452d7
GitHub Action <action@github.com>
2024-11-23 08:11:26 +0000

    Update locale files

--- src/i18n/TSH_zh-TW.ts
+++ src/i18n/TSH_zh-TW.ts
@@ -1450,7 +1450,7 @@ p, li { white-space: pre-wrap; }
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Helpers/TSHCountryHelper.py" line="182"/>
+        <location filename="../Helpers/TSHCountryHelper.py" line="184"/>
         <source>Custom Flags</source>
         <translation type="unfinished"></translation>
     </message>
