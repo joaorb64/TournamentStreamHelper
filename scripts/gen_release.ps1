99f5ab2173c7f3ee95918b451e07ff94695ac5f9
João Ribeiro Bezerra <joaorb64@gmail.com>
2024-12-20 15:50:54 +0000

    Final fix

--- scripts/gen_release.ps1
+++ scripts/gen_release.ps1
@@ -24,4 +24,6 @@ Copy-Item -Recurse -Force "user_data" "TournamentStreamHelper\user_data"
 Copy-Item -Force "LICENSE" "TournamentStreamHelper\LICENSE"
 Copy-Item -Force "TSH.exe" "TournamentStreamHelper\TSH.exe"
 
+Compress-Archive -Path "TournamentStreamHelper" -DestinationPath "release.zip" -Update
+
 Pop-Location
