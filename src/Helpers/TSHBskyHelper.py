79e232ce3fdf343430e6b14ebbd2d8a9f5c664a8
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2024-12-26 18:23:03 +0100

    Fixes

--- src/Helpers/TSHBskyHelper.py
+++ src/Helpers/TSHBskyHelper.py
@@ -5,10 +5,12 @@ from qtpy.QtGui import *
 from qtpy.QtWidgets import *
 from qtpy.QtCore import *
 from .TSHAltTextHelper import generate_bsky_text, generate_youtube
+from .TSHLocaleHelper import TSHLocaleHelper
 from src.SettingsManager import SettingsManager
 from atproto import Client
 
 def post_to_bsky(scoreboardNumber=1, image_path=None):
+    locale = TSHLocaleHelper.programLocale
     bsky_account = SettingsManager.Get("bsky_account", {})
     if not bsky_account or not bsky_account.get("username") or not bsky_account.get("host") or not bsky_account.get("app_password"):
         raise ValueError(QApplication.translate("app", "Bluesky account not correctly set"))
@@ -19,4 +21,4 @@ def post_to_bsky(scoreboardNumber=1, image_path=None):
     yt_title, yt_description = generate_youtube(scoreboard_id = scoreboardNumber)  # Use YouTube Description as Alt Text
 
     with open(image_path, "rb") as image:
-            post = client.send_images(builder, images=[image.read()], image_alts=[yt_description])
+            post = client.send_images(builder, images=[image.read()], image_alts=[yt_description], langs=[locale])
