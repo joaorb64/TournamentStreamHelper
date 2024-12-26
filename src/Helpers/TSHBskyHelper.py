1b939cd1b532cbd6480192aea56a88750cc7f8d3
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2024-12-26 19:03:16 +0100

    Added Settings widget

--- src/Helpers/TSHBskyHelper.py
+++ src/Helpers/TSHBskyHelper.py
@@ -15,7 +15,7 @@ def post_to_bsky(scoreboardNumber=1, image_path=None):
     if not bsky_account or not bsky_account.get("username") or not bsky_account.get("host") or not bsky_account.get("app_password"):
         raise ValueError(QApplication.translate("app", "Bluesky account not correctly set"))
     client = Client(bsky_account.get("host", "https://bsky.social"))
-    client.login(bsky_account["username"], bsky_account["app_password"])
+    client.login(bsky_account["username"].lstrip("@"), bsky_account["app_password"])
 
     raw_text, builder = generate_bsky_text(scoreboard_id = scoreboardNumber)
     yt_title, yt_description = generate_youtube(scoreboard_id = scoreboardNumber)  # Use YouTube Description as Alt Text
