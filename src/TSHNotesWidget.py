96a1982a2fb7fd6c1f4e01fe53577e9a429ad565
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2025-02-21 21:05:39 +0100

    Added Notes functionality

--- src/TSHNotesWidget.py
+++ src/TSHNotesWidget.py
@@ -20,6 +20,29 @@ import textwrap
 
 class TSHNotesWidget(QDockWidget):
     def __init__(self, *args, base="notes"):
+        def create_individual_notes_widget(index):
+            widget = QWidget()
+            widget.setObjectName(f"notes_widget_{i+1}")
+            widget.setLayout(QVBoxLayout())
+
+            notes_title = QLineEdit()
+            widget.layout().addWidget(notes_title)
+            notes_title.setPlaceholderText(QApplication.translate("app", "Title"))
+            notes_title.editingFinished.connect(
+                lambda element=notes_title: StateManager.Set(
+                        f"notes.{widget.objectName()}.title", element.text()))
+            notes_title.editingFinished.emit()
+
+            notes_contents = QPlainTextEdit()
+            widget.layout().addWidget(notes_contents)
+            notes_contents.textChanged.connect(
+                lambda element=notes_contents: 
+                    StateManager.Set(
+                        f"notes.{widget.objectName()}.contents", element.toPlainText()))
+            notes_contents.textChanged.emit()
+
+            return(widget)
+        
         super().__init__(*args)
         self.setWindowTitle(QApplication.translate("app", "Additional Notes"))
         self.setFloating(True)
@@ -30,3 +53,6 @@ class TSHNotesWidget(QDockWidget):
 
         self.setFloating(True)
         self.setWindowFlags(Qt.WindowType.Window)
+
+        for i in range(4):
+            self.widget.layout().addWidget(create_individual_notes_widget(i))
