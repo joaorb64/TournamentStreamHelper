e97d3a66be99bd7da36a06783a99322d7ad3a3e5
David Winings <wisp558@gmail.com>
2025-09-17 18:34:48 -0700

    Forgot to remove a debugging log statement.

--- stage_strike_app/src/ScoreboardPage/TextField.jsx
+++ stage_strike_app/src/ScoreboardPage/TextField.jsx
@@ -1,13 +1,12 @@
 import {styled} from "@mui/material";
 import {TextField as MuiTextField} from "@mui/material";
 
-const TextField = styled(MuiTextField)(({theme}) => {
-    console.log(theme);
-    return {
+const TextField = styled(MuiTextField)(({theme}) =>
+    ({
         '& label[data-shrink="false"]': {
-        color: theme.palette.text.disabled
-    }
-    }
-});
+            color: theme.palette.text.disabled
+        }
+    })
+);
 
 export default TextField;
