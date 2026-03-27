315f0a31fb62e7d61d16b04d024275512ef06aa9
Eion Dailey <eiondailey@live.com>
2026-03-27 19:34:20 -0400

    Hide Crew/Team Battle Widget for now (It shall return!)

--- src/TournamentStreamHelper.py
+++ src/TournamentStreamHelper.py
@@ -127,7 +127,7 @@ from .TSHHotkeys import TSHHotkeys
 from .TSHPlayerListWidget import TSHPlayerListWidget
 from .TSHNotesWidget import TSHNotesWidget
 from .TSHCommentaryWidget import TSHCommentaryWidget
-from .TSHTeamBattleWidget import TSHTeamBattleWidget
+# from .TSHTeamBattleWidget import TSHTeamBattleWidget
 from .TSHGameAssetManager import TSHGameAssetManager
 from .TSHBracketWidget import TSHBracketWidget
 from .TSHTournamentInfoWidget import TSHTournamentInfoWidget
@@ -416,13 +416,13 @@ class Window(QMainWindow):
             Qt.DockWidgetArea.BottomDockWidgetArea, tournamentInfo)
         self.dockWidgets.append(tournamentInfo)
 
-        teamBattle = TSHTeamBattleWidget()
-        teamBattle.setWindowIcon(QIcon('assets/icons/info.svg'))
-        teamBattle.setObjectName(
-            QApplication.translate("app", "Crew/Team Battle"))
-        self.addDockWidget(
-            Qt.DockWidgetArea.BottomDockWidgetArea, teamBattle)
-        self.dockWidgets.append(teamBattle)
+        # teamBattle = TSHTeamBattleWidget()
+        # teamBattle.setWindowIcon(QIcon('assets/icons/info.svg'))
+        # teamBattle.setObjectName(
+        #     QApplication.translate("app", "Crew/Team Battle"))
+        # self.addDockWidget(
+        #     Qt.DockWidgetArea.BottomDockWidgetArea, teamBattle)
+        # self.dockWidgets.append(teamBattle)
 
         self.scoreboard = TSHScoreboardManager.instance
         self.scoreboard.setWindowIcon(QIcon('assets/icons/list.svg'))
@@ -468,7 +468,7 @@ class Window(QMainWindow):
         self.tabifyDockWidget(self.scoreboard, self.stageWidget)
         self.tabifyDockWidget(self.scoreboard, commentary)
         self.tabifyDockWidget(self.scoreboard, tournamentInfo)
-        self.tabifyDockWidget(self.scoreboard, teamBattle)
+        # self.tabifyDockWidget(self.scoreboard, teamBattle)
         if not SettingsManager.Get("general.disable_thumbnail_widget", False):
             self.tabifyDockWidget(self.scoreboard, thumbnailSetting)
         self.tabifyDockWidget(self.scoreboard, playerList)
@@ -608,7 +608,7 @@ class Window(QMainWindow):
         if not SettingsManager.Get("general.disable_thumbnail_widget", False):
             toggleWidgets.addAction(thumbnailSetting.toggleViewAction())
         toggleWidgets.addAction(tournamentInfo.toggleViewAction())
-        toggleWidgets.addAction(teamBattle.toggleViewAction())
+        # toggleWidgets.addAction(teamBattle.toggleViewAction())
         toggleWidgets.addAction(playerList.toggleViewAction())
         toggleWidgets.addAction(bracket.toggleViewAction())
         toggleWidgets.addAction(notes.toggleViewAction())
